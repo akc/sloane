3e5db8a2e852dac2078121a4192946e2104271ee
Anders Claesson <anders.claesson@gmail.com>
2013-06-08 14:56:51 +0100

    Optionally print urls of entries found

--- sloane.hs
+++ sloane.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
--- Copyright   : (c) Anders Claesson 2012
--- License     : BSD-style
+-- |
+-- Copyright   : Anders Claesson 2012, 2013
 -- Maintainer  : Anders Claesson <anders.claesson@gmail.com>
+--
 
 import Prelude hiding (all)
 import System.Console.CmdArgs
 import Data.Maybe (fromJust)
-import Control.Monad (when)
+import Control.Monad (when, guard)
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
 import Network.URL (importURL, exportURL, add_param)
 
@@ -15,11 +16,13 @@ type OEISEntries = String
 type Query = String
 type Key = Char
 
-oeisURL = fromJust $ importURL "http://oeis.org/search?fmt=text"
+oeisHost = "http://oeis.org"
+oeisURL  = fromJust . importURL $ oeisHost ++ "/search?fmt=text"
 oeisKeys = "ISTUVWXNDHFYAOEeptoKC"
 
 data Sloane = Sloane { keys  :: String
                      , all   :: Bool
+                     , url   :: Bool
                      , limit :: Int
                      , terms :: String
                      }
@@ -27,15 +30,23 @@ data Sloane = Sloane { keys  :: String
 
 sloane = cmdArgsMode $ Sloane 
          { keys  = "SN"  &= typ "KEYS" &= help "Keys of fields to print (default: SN)"
-         , all   = False               &= help "Print all fields"
+         , all   = False &= name "a"   &= help "Print all fields"
+         , url   = False &= name "u"   &= help "Print urls of found entries"
          , limit = 5     &= name "n"   &= help "Limit the number of entries retrieved (default: 5)"
          , terms = def   &= argPos 0   &= typ "SEARCH-TERMS"
          }
-         &= versionArg [summary "sloane 0.1.4"]
+         &= versionArg [summary "sloane 0.1.5"]
          &= summary "Search Sloane's On-Line Encyclopedia of Integer Sequences"
 
 select :: [Key] -> OEISEntries -> OEISEntries
 select ks = unlines . filter (\xs -> null xs || head xs `elem` ks) . lines
+
+urls :: OEISEntries -> String
+urls es = unlines $ do
+            ids <- lines $ select "I" es
+            guard $ not (null ids)
+            let aNum = (words ids) !! 1
+            return $ oeisHost ++ "/" ++ aNum
 
 searchOEIS :: Int -> Query -> IO OEISEntries
 searchOEIS n s = do
@@ -49,4 +60,8 @@ main = do
   result <- searchOEIS (limit args) . filter (`notElem` "[]") $ terms args
   when (not $ null result) $ do
     putStrLn ""
-    putStrLn $ if all args then result else select (keys args) result
+    if (url args)
+      then
+        putStrLn $ urls result
+      else
+        putStrLn $ if all args then result else select (keys args) result
