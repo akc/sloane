16513dfd312315b8a21cdef29e94ee9ae720a688
Anders Claesson <akc@akc.is>
2013-10-28 00:48:21 +0000

    Fix problem with UTF8

--- sloane.hs
+++ sloane.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 -- |
--- Copyright   : Anders Claesson 2012, 2013
+-- Copyright   : Anders Claesson 2012, 2013, 2014
 -- Maintainer  : Anders Claesson <anders.claesson@gmail.com>
 -- License     : BSD-3
 --
-
-import Prelude hiding (all)
+import Prelude hiding (all, putStrLn)
+import Data.ByteString.Char8 (putStrLn, pack)
 import System.Console.CmdArgs
 import Data.Maybe (fromJust)
-import Control.Monad (when, guard)
+import Control.Monad (unless, guard)
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
 import Network.URL (importURL, exportURL, add_param)
 
@@ -29,15 +29,15 @@ data Sloane = Sloane { keys  :: String
                      }
               deriving (Data, Typeable)
 
-sloane = cmdArgsMode $ Sloane 
-         { keys  = "SN"  &= typ "KEYS" &= help "Keys of fields to print (default: SN)"
-         , all   = False &= name "a"   &= help "Print all fields"
-         , url   = False &= name "u"   &= help "Print urls of found entries"
-         , limit = 5     &= name "n"   &= help "Limit the number of entries retrieved (default: 5)"
-         , terms = def   &= argPos 0   &= typ "SEARCH-TERMS"
-         }
-         &= versionArg [summary "sloane 0.1.5"]
-         &= summary "Search Sloane's On-Line Encyclopedia of Integer Sequences"
+sloane = cmdArgsMode $ Sloane
+  { keys  = "SN"  &= typ "KEYS" &= help "Keys of fields to print (default: SN)"
+  , all   = False &= name "a"   &= help "Print all fields"
+  , url   = False &= name "u"   &= help "Print urls of found entries"
+  , limit = 5     &= name "n"   &= help "Limit the number of entries retrieved (default: 5)"
+  , terms = def   &= argPos 0   &= typ "SEARCH-TERMS"
+  }
+  &= versionArg [summary "sloane 1.1"]
+  &= summary "Search Sloane's On-Line Encyclopedia of Integer Sequences"
 
 select :: [Key] -> OEISEntries -> OEISEntries
 select ks = unlines . filter (\xs -> null xs || head xs `elem` ks) . lines
@@ -46,11 +46,11 @@ urls :: OEISEntries -> String
 urls es = unlines $ do
             ids <- lines $ select "I" es
             guard $ not (null ids)
-            let aNum = (words ids) !! 1
+            let aNum = words ids !! 1
             return $ oeisHost ++ "/" ++ aNum
 
 searchOEIS :: Int -> Query -> IO OEISEntries
-searchOEIS n s = do
+searchOEIS n s =
   trim `fmap` (simpleHTTP (getRequest url) >>= getResponseBody)
     where
       trim = unlines . map (drop 1) . reverse . drop 2 . reverse . drop 5 . lines
@@ -58,11 +58,7 @@ searchOEIS n s = do
 
 main = do
   args <- cmdArgsRun sloane
-  result <- searchOEIS (limit args) . filter (`notElem` "[]") $ terms args
-  when (not $ null result) $ do
-    putStrLn ""
-    if (url args)
-      then
-        putStrLn $ urls result
-      else
-        putStrLn $ if all args then result else select (keys args) result
+  entries <- searchOEIS (limit args) . filter (`notElem` "[]") $ terms args
+  let pick = if all args then id else select (keys args)
+  unless (null entries) $
+         putStrLn . pack $ '\n' : (if url args then urls else pick) entries
