b3c896019f79daaf5a3f617b00f8e4742508b4fa
Anders Claesson <akc@akc.is>
2014-03-08 11:28:51 +0000

    Refactor

--- sloane.hs
+++ sloane.hs
@@ -13,10 +13,10 @@ import Control.Monad (unless, guard)
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
 import Network.URL (importURL, exportURL, add_param)
 
-type OEISEntries = String
-type ANumber = String
+type OEISEntries = [String]
+type ANumbers = [String]
 type Query = String
-type Key = Char
+type Keys = String
 
 oeisHost = "http://oeis.org"
 oeisURL  = fromJust . importURL $ oeisHost ++ "/search?fmt=text"
@@ -31,35 +31,35 @@ data Sloane = Sloane { keys  :: String
               deriving (Data, Typeable)
 
 sloane = cmdArgsMode $ Sloane
-  { keys  = "SN"  &= typ "KEYS" &= help "Keys of fields to print (default: SN)"
-  , all   = False &= name "a"   &= help "Print all fields"
-  , url   = False &= name "u"   &= help "Print urls of found entries"
-  , limit = 5     &= name "n"   &= help "Retrieve at most this many entries (default: 5)"
-  , terms = def   &= argPos 0   &= typ "SEARCH-TERMS"
+  { keys = "SN"  &= typ "KEYS" &= help "Keys of fields to print (default: SN)"
+  , all  = False &= name "a"   &= help "Print all fields"
+  , url  = False &= name "u"   &= help "Print urls of found entries"
+  , limit = 5 &= name "n" &= help "Retrieve at most this many entries (default: 5)"
+  , terms = def &= argPos 0 &= typ "SEARCH-TERMS"
   }
   &= versionArg [summary "sloane 1.1"]
   &= summary "Search Sloane's On-Line Encyclopedia of Integer Sequences"
 
-select :: [Key] -> OEISEntries -> OEISEntries
-select ks = unlines . filter (\line -> null line || head line `elem` ks) . lines
+select :: Keys -> OEISEntries -> OEISEntries
+select ks = filter (\line -> null line || head line `elem` ks)
 
-aNumbers :: OEISEntries -> [ANumber]
-aNumbers es = [ words ids !! 1 | ids@(_:_) <- lines (select "I" es) ]
+aNumbers :: OEISEntries -> ANumbers
+aNumbers es = [ words ids !! 1 | ids@(_:_) <- select "I" es ]
 
-urls :: OEISEntries -> String
-urls = unlines . map ((oeisHost ++ "/") ++ ) . aNumbers
+urls :: OEISEntries -> [String]
+urls = map ((oeisHost ++ "/") ++ ) . aNumbers
 
 searchOEIS :: Int -> Query -> IO OEISEntries
 searchOEIS n s =
   trim `fmap` (simpleHTTP (getRequest url) >>= getResponseBody)
     where
-      trim = unlines . map (drop 1) . reverse . drop 2 . reverse . drop 5 . lines
+      trim = map (drop 1) . reverse . drop 2 . reverse . drop 5 . lines
       url = exportURL $ oeisURL `add_param` ("n", show n) `add_param` ("q", s)
 
 main = do
   args <- cmdArgsRun sloane
   let query = filter (`notElem` "[{}]") $ terms args
-  entries <- searchOEIS (limit args) query
+  hits <- searchOEIS (limit args) query
   let pick = if all args then id else select (keys args)
-  unless (null entries) $
-         putStrLn . pack $ '\n' : (if url args then urls else pick) entries
+  unless (null hits) $
+      putStrLn . pack $ '\n' : unlines ((if url args then urls else pick) hits)
