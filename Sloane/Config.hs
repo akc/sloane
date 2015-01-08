55862acedc3116df9b4bc9658877ebfe11ddb21f
Anders Claesson <akc@akc.is>
2015-01-08 13:37:17 +0000

    version++

--- Sloane/Config.hs
+++ Sloane/Config.hs
@@ -29,7 +29,7 @@ defaultConfig = do
     h <- getHomeDirectory
     let dsloane = h </> ".sloane"
     return Config
-        { nameVer   = "sloane 2.0.1"
+        { nameVer   = "sloane 2.0.2"
         , home      = h
         , sloaneDir = dsloane
         , sloaneDB  = dsloane </> "sloane.db"
