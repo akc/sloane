6763e65fbab62002228325a7051a217151763439
Anders Claesson <akc@akc.is>
2015-02-04 15:10:14 +0000

    New command line option --nocolor. Also rename a couple of other options.

--- sloane.hs
+++ sloane.hs
@@ -30,19 +30,21 @@ import Sloane.Config
 import Sloane.Transform
 
 data Options = Options
-    { full    :: Bool     -- Print all fields?
-    , keys    :: String   -- Keys of fields to print
-    , limit   :: Int      -- Fetch at most this many entries
-    , url     :: Bool     -- Print URLs of found entries
-    , local   :: Bool     -- Lookup in local DB
-    , anumber :: Int      -- Get the sequence with this number from the local DB
-    , filtr   :: Bool     -- Filter out sequences in local DB
-    , invert  :: Bool     -- Return sequences NOT in DB
-    , transform :: String -- Apply the named transform
-    , listTransforms :: Bool -- List the names of all transforms
-    , update  :: Bool     -- Updated local DB
-    , version :: Bool     -- Show version info
-    , terms   :: [String] -- Search terms
+    { longFormat     :: Bool     -- Print all fields?
+    , keys           :: String   -- Keys of fields to print
+    , limit          :: Int      -- Fetch at most this many entries
+    , limitless      :: Bool     -- Fetch all entries (that match)
+    , local          :: Bool     -- Lookup in local DB
+    , url            :: Bool     -- Print URLs of found entries
+    , nocolor        :: Bool     -- Do not colorize the output
+    , anumber        :: Int      -- Get this sequence from the local DB
+    , filtr          :: Bool     -- Filter out sequences in local DB
+    , invert         :: Bool     -- Return sequences NOT in DB
+    , transform      :: String   -- Apply the named transform
+    , listTransforms :: Bool     -- List the names of all transforms
+    , update         :: Bool     -- Updated local DB
+    , version        :: Bool     -- Show version info
+    , terms          :: [String] -- Search terms
     }
 
 aNums :: Reply -> [ANum]
@@ -82,19 +84,19 @@ readNamesDB cfg = B.readFile (namesDBPath cfg)
 mkReply :: SeqMap -> NamesMap -> [ANum] -> Reply
 mkReply s n = M.fromList . map (\k -> (k, M.fromList [('S', [s!k]), ('N', [n!k])]))
 
-putReply :: Config -> [Key] -> Reply -> IO ()
-putReply cfg ks reply = do
+putReply :: Config -> Bool -> [Key] -> Reply -> IO ()
+putReply cfg noColor ks reply = do
     unless (M.null reply) $ putStrLn ""
     forM_ (M.toList reply) $ \(anum, fields) -> do
         forM_ (ks `intersect` M.keys fields) $ \key -> do
             let entry = fields ! key
             forM_ entry $ \line -> do
                 let line' = decodeUtf8 line
-                setSGR [ SetColor Foreground Dull Green ]
+                unless noColor $ setSGR [ SetColor Foreground Dull Green ]
                 putStr [key]
-                setSGR [ SetColor Foreground Dull Yellow ]
+                unless noColor $ setSGR [ SetColor Foreground Dull Yellow ]
                 putStr " " >> B.putStr (packANum anum)
-                setSGR []
+                unless noColor $ setSGR []
                 putStr " " >> IO.putStrLn (crop key (termWidth cfg - 10) line')
         putStrLn ""
   where
@@ -104,10 +106,13 @@ putReply cfg ks reply = do
         then cropText $ \cap -> T.reverse . T.dropWhile (/= ',') . T.reverse . T.take cap
         else cropText $ \cap s -> T.take (cap-2) s `T.append` T.pack ".."
 
+keys' :: Options -> String
+keys' opts = if longFormat opts then oeisKeys else keys opts
+
 putReply' :: Options -> Config -> Reply -> IO ()
 putReply' opts cfg
     | url opts  = putStr . unlines . oeisUrls cfg
-    | otherwise = putReply cfg (if full opts then oeisKeys else keys opts)
+    | otherwise = putReply cfg (nocolor opts) (keys' opts)
 
 oeisUrls :: Config -> Reply -> [URL]
 oeisUrls cfg = map ((oeisHost cfg ++) . B.unpack . packANum) . aNums
@@ -126,9 +131,9 @@ applyTransform opts tname =
                        [] -> return ()
                        cs -> B.putStrLn (packIntSeq cs)
 
-getTerms :: Options -> IO Options
-getTerms opts
-    | dont      = return opts
+terms' :: Options -> IO Options
+terms' opts
+    | doNothing = return opts
     | otherwise = (\xs -> opts {terms = xs}) <$>
         case terms opts of
           [] -> isEOF >>= \b ->
@@ -136,12 +141,8 @@ getTerms opts
                        else lines <$> getContents
           ts -> return ts
   where
-    dont = or [ filtr opts
-              , anumber opts > 0
-              , listTransforms opts
-              , update opts
-              , version opts
-              ]
+    doNothing = version opts || anumber opts > 0 || filtr opts
+              || update opts || listTransforms opts
 
 takeUniq :: Eq a => Int -> [a] -> [a]
 takeUniq n = take n . map head . group
@@ -161,9 +162,9 @@ optionsParser :: Parser Options
 optionsParser =
   abortOption ShowHelpText (long "help") <*> (Options
     <$> switch
-        ( short 'a'
-       <> long "all"
-       <> help "Print all fields" )
+        ( short 'l'
+       <> long "long"
+       <> help "Long format; print all fields" )
     <*> strOption
         ( short 'k'
        <> metavar "KEYS"
@@ -175,11 +176,20 @@ optionsParser =
        <> value 5
        <> help "Fetch at most this many entries [default: 5]" )
     <*> switch
+        ( short 'a'
+       <> long "all"
+       <> help "Fetch all matching entries (equivalent to -n 999999)" )
+    <*> switch
+        ( short 'q'
+       <> long "quick"
+       <> help ("Do a 'quick' offline search for a sequence using the local"
+            ++ " database rather than oeis.org" ) )
+    <*> switch
         ( long "url"
        <> help "Print URLs of found entries" )
     <*> switch
-        ( long "local"
-       <> help "Use the local database rather than oeis.org" )
+        ( long "nocolor"
+       <> help "Do not colorize the output" )
     <*> option auto
         ( short 'A'
        <> metavar "NUMBER"
@@ -194,10 +204,11 @@ optionsParser =
        <> help ("Return sequences NOT in the database;"
             ++ " only relevant when used with --filter") )
     <*> strOption
-        ( long "transform"
+        ( short 't'
+       <> long "transform"
        <> metavar "NAME"
        <> value ""
-       <> help "Apply the named transform to input sequence" )
+       <> help "Apply the named transform to input sequences" )
     <*> switch
         ( long "list-transforms"
        <> help "List the names of all transforms" )
@@ -212,11 +223,11 @@ optionsParser =
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
-    opts <- getTerms =<< execParser (info optionsParser fullDesc)
-    let n = limit opts
+    opts <- terms' =<< execParser (info optionsParser fullDesc)
+    let n = if limitless opts then 999999 else limit opts
     let ts = terms opts
-    let tname = transform opts
     let anum = anumber opts
+    let tname = transform opts
     let lookupSeq = maybeToList . M.lookup anum
     let sloane c
          | version opts = putStrLn (nameVer c)
