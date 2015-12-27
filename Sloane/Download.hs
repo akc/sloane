2ea77a5b10ee7b7e27c4146be13dcf70d34a71c9
Anders Claesson <akc@akc.is>
2015-12-27 22:39:09 +0000

    Cater for different versions of http_conduit

--- Sloane/Download.hs
+++ Sloane/Download.hs
@@ -16,21 +16,13 @@ import qualified Data.Conduit.Binary as CB
 import Network.HTTP.Conduit hiding (Proxy)
 import Network.HTTP.Types (hContentLength)
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Resource (runResourceT)
-#if __GLASGOW_HASKELL__ < 710
-import Control.Applicative
-#endif
 import Sloane.OEIS (URL)
 import System.Console.ANSI
 import System.IO
 
--- | Request a page at a given URL with specified key-value pairs (query
--- string).
-requestPage :: URL -> [(ByteString, ByteString)] -> IO ByteString
-requestPage url kvs = do
-    req <- liftIO $ setQueryString [(k, Just v) | (k,v) <- kvs] <$> parseUrl url
-    man <- newManager tlsManagerSettings
-    BL.toStrict . responseBody <$> httpLbs req man
+#if MIN_VERSION_http_conduit(2,1,7)
+
+import Control.Monad.Trans.Resource (runResourceT)
 
 -- | Download a file at a given URL showing a progress indicator at the
 -- given column, and save it at a specified path.
@@ -52,3 +44,41 @@ download col url fpath = do
         liftIO $ hFlush stdout
         yield chunk
         progress total acc')
+
+#else
+
+#if !MIN_VERSION_base(4,8,0)
+import Control.Applicative
+#endif
+
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+-- | Download a file at a given URL showing a progress indicator at the
+-- given column, and save it at a specified path.
+download :: Int -> URL -> FilePath -> IO ()
+download col url fpath = withManager $ \manager -> do
+    req <- parseUrl url
+    res <- http req manager
+    let Just cl = lookup hContentLength (responseHeaders res)
+    let n = read (B.unpack cl) :: Int
+    responseBody res $$+- progress n 0 =$ ungzip =$ CB.sinkFile fpath
+  where
+    progress total acc = await >>= maybe (return ()) (\chunk -> do
+        let acc' = acc + B.length chunk
+        let percent = fromIntegral (100*acc') / fromIntegral total :: Double
+        liftIO $ setCursorColumn col
+        liftIO $ putStr $ showFFloat (Just 2) percent "" ++ "%"
+        liftIO $ hFlush stdout
+        yield chunk
+        progress total acc')
+
+#endif
+
+-- | Request a page at a given URL with specified key-value pairs (query
+-- string).
+requestPage :: URL -> [(ByteString, ByteString)] -> IO ByteString
+requestPage url kvs = do
+    req <- liftIO $ setQueryString [(k, Just v) | (k,v) <- kvs] <$> parseUrl url
+    man <- newManager tlsManagerSettings
+    BL.toStrict . responseBody <$> httpLbs req man
